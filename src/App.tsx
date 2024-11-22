d28c4a21fc3b40219e76844d5f2856218ea3e3fc
yousseffdil <fdilyoussef@gmail.com>
2024-11-22 09:06:53 +0100

    Add Vercel Analytics integration and update package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import PortFolio from './Components/Portfolio'
 import './App.css'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
       <PortFolio />
+      <Analytics/>
     </>
   )
 }
